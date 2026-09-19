5c1bf655baa2b2d082d9df89552c9e4f0c053696
dpk404 <dk950046@gmail.com>
2026-09-19 17:27:59 +0530

    chore: bump version to 2.0.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.remotephone"
         minSdk = 24
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.3.0"
+        versionCode = 7
+        versionName = "2.0.0"
     }
 
     signingConfigs {
